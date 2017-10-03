3996b34ed2f18676bcbbad59727a0781857862df
Alex M <oblivionpick@gmail.com>
2017-10-03 14:51:37 +0200

    removed unnecessary newlines

--- test.php
+++ test.php
@@ -44,14 +44,6 @@ var_dump($inget);
 echo "</pre>";
 
 
-
-
-
-
-
-
-
-
 echo "<p>Dagens datum och tid är nu " . date('r') . "</p>";
 echo "<p>Så här många sekunder har det gått sedan 
 den förste januari 1970: " . time() . "</p>";
